67235076f71e94cd8ae9be2a66d7f76a916fa4f3
Dima Gerasimov <karlicoss@gmail.com>
2019-07-29 07:41:54 +0100

    tmp work on using TweetScraper

--- axol/twitter.py
+++ axol/twitter.py
@@ -1,37 +1,86 @@
-import scrapy # type: ignore
-from scrapy.crawler import CrawlerProcess # type: ignore
+from datetime import datetime
+from typing import List, Dict, NamedTuple, Optional, Iterator
+import logging
 
 
-from kython import import_from
+def get_logger():
+    return logging.getLogger('twisearch')
 
-# scrape = import_from('/L/zzz_syncthing/tmp/TweetScraper', 'TweetScraper')
-from TweetScraper.spiders.TweetCrawler import TweetScraper
 
-from scrapy.settings import Settings
+class Result(NamedTuple):
+    uid: str
+    when: datetime
+    link: str
+    text: str
+    user: str
+    # TODO fixme not sure about title?..
+    # pass
 
-items = []
 
-# pipeline to fill the items list
-class ItemCollectorPipeline(object):
-    def __init__(self):
-        self.ids_seen = set()
-        print("INSTANTIATING!")
 
+_items = []
+
+class AppendToList(object):
     def process_item(self, item, spider):
-        items.append(item)
+        _items.append(item)
 
-def main():
+def scrapy_search(query: str):
+    assert len(_items) == 0
+
+    from scrapy.crawler import CrawlerProcess # type: ignore
+    from kython import import_from
+    scrape = import_from('/L/zzz_syncthing/tmp/TweetScraper', 'TweetScraper')
+    print(dir(scrape))
+    from TweetScraper.spiders.TweetCrawler import TweetScraper
+
     process = CrawlerProcess({
-        'ITEM_PIPELINES': { '__main__.ItemCollectorPipeline': 100 },  # TODO what does the number mean?
+        'ITEM_PIPELINES': {__name__ + '.AppendToList': 100},
         'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
         'LOG_LEVEL': 'DEBUG',
         'DUPEFILTER_DEBUG': True,
     })
-    process.crawl(TweetScraper, query='"виктор аргонов"')
+    process.crawl(TweetScraper, query=query)
     process.start() # blocking
-    for i in items:
-        import ipdb; ipdb.set_trace() 
-        print(i)
+    # TODO FIXME inject some callback into reactor?
+    try:
+        return [i for i in _items]
+    finally:
+        _items.clear()
+
+
+class TwitterSearch:
+    def __init__(self) -> None:
+        self.logger = get_logger()
+        # TODO FIXME delay?
+        # self.d
+
+    def iter_search(self, query, limit=None) -> Iterator[Result]:
+        # TODO FIXME not sure if we can be iterative here..
+        # TODO FIXME respect limit if we make it iterative?
+        for i in scrapy_search(query=query):
+            yield Result(
+                uid =i['ID'],
+                when=i['datetime'], # TODO FIXME # TO
+                link=i['url'],
+                text=i['text'],
+                user=i['usernameTweet'], # TOOD not sure if need to keep user_id
+                # TODO FIXME fav/rt stats?
+            )
+
+    def search(self, query: str, limit=None) -> List[Result]:
+        # TODO FIXME do I need to sort anything?
+        return list(self.iter_search(query=query, limit=limit))
+
+    # TODO FIXME search_all gonna look very similar?
+    def search_all(self, queries: List[str], limit=None) -> List[Result]:
+        assert len(queries) == 1 # TODO FIXME
+        return self.search(query=queries[0], limit=limit)
+
+
+def main():
+    ts = TwitterSearch()
+    for r in ts.search('"quantified self"'):
+        print(r)
 
 # TODO set some limit?
 # {'ID': '1146731105485250562',
