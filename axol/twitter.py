1135816cc863508a19697e24136d1e34223d905b
Dima Gerasimov <karlicoss@gmail.com>
2019-07-29 07:57:59 +0100

    use twitterscraper

--- axol/twitter.py
+++ axol/twitter.py
@@ -14,38 +14,40 @@ class Result(NamedTuple):
     text: str
     user: str
     # TODO fixme not sure about title?..
-    # pass
+    replies: int
+    retweets: int
+    likes: int
 
 
-
-_items = []
-
-class AppendToList(object):
-    def process_item(self, item, spider):
-        _items.append(item)
-
-def scrapy_search(query: str):
-    assert len(_items) == 0
-
-    from scrapy.crawler import CrawlerProcess # type: ignore
-    from kython import import_from
-    scrape = import_from('/L/zzz_syncthing/tmp/TweetScraper', 'TweetScraper')
-    print(dir(scrape))
-    from TweetScraper.spiders.TweetCrawler import TweetScraper
-
-    process = CrawlerProcess({
-        'ITEM_PIPELINES': {__name__ + '.AppendToList': 100},
-        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-        'LOG_LEVEL': 'DEBUG',
-        'DUPEFILTER_DEBUG': True,
-    })
-    process.crawl(TweetScraper, query=query)
-    process.start() # blocking
-    # TODO FIXME inject some callback into reactor?
-    try:
-        return [i for i in _items]
-    finally:
-        _items.clear()
+# TODO move this to scrapy_singlefile (and rename to 'adhoc'??)
+# _items = []
+#
+# class AppendToList(object):
+#     def process_item(self, item, spider):
+#         _items.append(item)
+#
+# def scrapy_search(query: str):
+#     assert len(_items) == 0
+#
+#     from scrapy.crawler import CrawlerProcess # type: ignore
+#     from kython import import_from
+#     scrape = import_from('/L/zzz_syncthing/tmp/TweetScraper', 'TweetScraper')
+#     print(dir(scrape))
+#     from TweetScraper.spiders.TweetCrawler import TweetScraper
+#
+#     process = CrawlerProcess({
+#         'ITEM_PIPELINES': {__name__ + '.AppendToList': 100},
+#         'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+#         'LOG_LEVEL': 'DEBUG',
+#         'DUPEFILTER_DEBUG': True,
+#     })
+#     process.crawl(TweetScraper, query=query)
+#     process.start() # blocking
+#     # TODO FIXME inject some callback into reactor?
+#     try:
+#         return [i for i in _items]
+#     finally:
+#         _items.clear()
 
 
 class TwitterSearch:
@@ -55,16 +57,18 @@ class TwitterSearch:
         # self.d
 
     def iter_search(self, query, limit=None) -> Iterator[Result]:
+        from twitterscraper import query_tweets # type: ignore
         # TODO FIXME not sure if we can be iterative here..
-        # TODO FIXME respect limit if we make it iterative?
-        for i in scrapy_search(query=query):
+        for i in sorted(query_tweets(query, limit=limit)):
             yield Result(
-                uid =i['ID'],
-                when=i['datetime'], # TODO FIXME # TO
-                link=i['url'],
-                text=i['text'],
-                user=i['usernameTweet'], # TOOD not sure if need to keep user_id
-                # TODO FIXME fav/rt stats?
+                uid =i.id,
+                when=i.timestamp, # no tz, apparently local scraping tz?
+                link=i.url,
+                text=i.text,
+                user=i.user, # TOOD not sure if need to keep user_id
+                replies=i.replies,
+                retweets=i.retweets,
+                likes=i.likes,
             )
 
     def search(self, query: str, limit=None) -> List[Result]:
@@ -79,27 +83,8 @@ class TwitterSearch:
 
 def main():
     ts = TwitterSearch()
-    for r in ts.search('"quantified self"'):
+    for r in ts.search('"виктор аргонов"', limit=10):
         print(r)
-
-# TODO set some limit?
-# {'ID': '1146731105485250562',
-#  'datetime': '2019-07-04 11:42:55',
-#  'has_media': True,
-#  'is_reply': False,
-#  'is_retweet': False,
-#  'medias': ['https://t.co/TV9SPFJUFO'],
-#  'nbr_favorite': 3,
-#  'nbr_reply': 2,
-#  'nbr_retweet': 0,
-#  'text': 'В общем, знайте, что есть только один канон Русалочки\n'
-#          '\n'
-#          ' Виктор Аргонов  Project — Сделать шаг (Ария сказочницы)  https:// '
-#          'music.yandex.ru/album/3639147/ track/30168045 \xa0 …',
-#  'url': '/kazdalevsky/status/1146731105485250562',
-#  'user_id': '159019766',
-#  'usernameTweet': 'kazdalevsky'}
-
 
 
 if __name__ == '__main__':
