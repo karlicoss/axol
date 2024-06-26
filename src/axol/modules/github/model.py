67241152b9d333104887276dc9922802820bc7b9
Dima Gerasimov <karlicoss@gmail.com>
2024-06-26 21:38:15 +0100

    github model: process remaining entities

--- src/axol/modules/github/model.py
+++ src/axol/modules/github/model.py
@@ -7,49 +7,61 @@ from axol.core.common import datetime_aware, Json, _check
 @dataclass
 class Base:
     created_at: datetime_aware | None
+    html_url: str
 
 
 @dataclass
 class Commit(Base):
-    html_url: str
     message: str
     author_login: str | None
 
 
 @dataclass
 class Code(Base):
-    html_url: str
+    pass
+
+
+@dataclass
+class Issue(Base):
+    title: str
+    body: str
+    # todo total reactions count?
+    user_login: str | None
 
 
 @dataclass
 class Repository(Base):
-    pass
+    description: str | None
+    topics: tuple[str]
+    stars: int
 
 
-Result = Code | Commit | Repository
+Result = Code | Commit | Issue | Repository
 
 
 def parse(j: Json) -> Result:
-    orig_j = j
-    # FIXME ugh.. this will still modify due to nested dicts
     j = {k: v for k, v in j.items()}
-    print(j.keys())
     entity_types = []
-    # FIXME hmm might be easier to add entity type during search?
+    # todo hmm might be easier to add entity type during search?
     if 'path' in j:
         entity_types.append('code')
     if 'commit' in j:
         entity_types.append('commit')
+    if 'state' in j:
+        entity_types.append('issue')
+    if 'forks_count' in j:
+        entity_types.append('repository')
     assert len(entity_types) == 1, (entity_types, j)
     [entity_type] = entity_types
 
-    j['repository'] = {
-        # filter out weird 'templates'
-        # TODO maybe delete them during scraping
-        k: v
-        for k, v in j['repository'].items()
-        if not isinstance(v, str) or not ('api.github.com' in v and '{' in v and '}' in v)
-    }
+    # todo later -- issues don't have it, only repository url?
+    # j['repository'] = {
+    #     # filter out weird 'templates'
+    #     # TODO maybe delete them during scraping
+    #     k: v
+    #     for k, v in j['repository'].items()
+    #     if not isinstance(v, str) or not ('api.github.com' in v and '{' in v and '}' in v)
+    # }
 
     html_url = _check(j.pop('html_url'), str)
 
@@ -79,6 +91,35 @@ def parse(j: Json) -> Result:
             html_url=html_url,
             message=message,
             author_login=author_login,
+        )
+    elif entity_type == 'issue':
+        title = _check(j.pop('title'), str)
+        body = _check(j.pop('body'), str)
+        created_at = datetime.fromisoformat(j.pop('created_at'))
+        user_login: str | None
+        user = j.pop('user')
+        if user is None:
+            user_login = None
+        else:
+            user_login = _check(user.pop('login'), str)
+        return Issue(
+            created_at=created_at,
+            html_url=html_url,
+            title=title,
+            body=body,
+            user_login=user_login,
+        )
+    elif entity_type == 'repository':
+        created_at = datetime.fromisoformat(j.pop('created_at'))
+        description = j.pop('description')  # can be none if empty
+        topics = tuple(sorted(j.pop('topics')))
+        stars = _check(j.pop('stargazers_count'), int)
+        return Repository(
+            created_at=created_at,
+            html_url=html_url,
+            description=description,
+            topics=topics,
+            stars=stars,
         )
     else:
         raise RuntimeError(entity_type)
