3ac68854feab57ec7e5cd746c0345c1a5246e785
Dima Gerasimov <karlicoss@gmail.com>
2024-08-01 22:43:01 +0100

    port initial markdown renderer from axol2zulip

---
+++ src/axol/modules/hackernews/markdown.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+
+
+from ...core.common import datetime_aware
+from .model import Result, Comment, Story
+from ...renderers.markdown import Author, MarkdownAdapterT, from_html, make_title
+
+
+# TODO should probably return a typed dict which is ready to be converted in message?
+# not sure
+@dataclass
+class MarkdownAdapter(MarkdownAdapterT):
+    model: Result
+
+    @property
+    def created_at(self) -> datetime_aware:
+        return self.model.created_at
+
+    @property
+    def author(self) -> Author:
+        name = self.model.author
+        return Author(
+            domain='news.ycombinator.com',
+            kind='Hackernews',
+            name=name,
+            # FIXME move to common for hn? same with domain
+            url=f'https://news.ycombinator.com/user?id={name}',
+        )
+
+    @property
+    def content(self) -> str:
+        o = self.model
+
+        if isinstance(o, Comment):
+            title = 'comment'
+            url = None
+        else:
+            title = o.title
+            url = o.url
+
+        pparts = []
+        if isinstance(o, Story):
+            if o.points > 0:
+                pparts.append(f'{o.points}⇧')
+            if o.num_comments > 0:
+                pparts.append(f'{o.num_comments}🗨')
+        prefix = None if len(pparts) == 0 else ' '.join(pparts)
+        title_line = make_title(prefix=prefix, title=title, permalink=o.permalink, url=url)
+
+        # FIXME get rid of footer?
+        parts: list[str] = []
+        footer = ', '.join(parts)
+
+        body = '' if o.text is None else from_html(o.text)
+
+        content = '\n'.join([title_line, body, footer])
+        # TODO could add emoji if it has comments/upvotes etc?
+        # yeah, e.g. each provider could decorate somehow? maybe according to percentile?
+        # e.g. 90+ % -- fire emoji or something
+        return content
---
+++ src/axol/renderers/markdown.py
@@ -0,0 +1,93 @@
+from abc import abstractmethod
+from dataclasses import dataclass
+from html import unescape as html_unescape
+import re
+from typing import Any
+
+import html2text
+
+from ..core.common import datetime_aware, html
+
+
+@dataclass
+class Author:
+    domain: str
+    kind: str
+    name: str
+    url: str
+
+    def __post_init__(self) -> None:
+        # FIXME need to sanitize/validate properly (have a helper in hpi2zulip)
+        # NOTE: can't contain square brackets
+        assert re.fullmatch(r'[\w\.-]+', self.name), self
+        assert self.url.startswith('http'), self
+
+
+# TODO hmm mypy is not warning about notimplemented??
+class MarkdownAdapterT:
+    @abstractmethod
+    def __init__(self, model: Any) -> None:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def created_at(self) -> datetime_aware | None:
+        """
+        When item was created (this is different from crawl datetime).
+        Sometimes this information isn't available, in this case return None and leave for the consumer to decide what to do.
+        """
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def author(self) -> Author:
+        """
+        Typically items have a well defined author, it can be useful for rendering to separate it out from the body
+        """
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def content(self) -> str:
+        # TODO split in header and footer? not sure.. or return a dataclass containing them?
+        raise NotImplementedError
+
+
+def from_html(text: html) -> str:
+    html_text = text.html
+    htext = html_unescape(html_text)
+    converter = html2text.HTML2Text(
+        bodywidth=1000,  # default in 78
+    )
+    # by default uses "_"
+    # which isn't supported in zulip yet https://github.com/zulip/zulip/issues/12325
+    converter.emphasis_mark = '*'
+    body = converter.handle(htext)
+    return body
+
+
+# TODO might need to sanitize title
+# maybe use some proper md renderer?
+# TODO maybe separate title out of body?
+def make_title(
+    *,
+    title: str,
+    permalink: str,
+    url: str | None,
+    prefix: str | None = None,
+) -> str:
+    # TODO add 'extra', insert before direct url?
+    # ugh. kinda confusing, but not sure how to name better
+    # permalink is the link to service itself (e.g. reddit/hn)
+    # url is the link to the site it's referring to
+    assert len(title.strip()) > 0, title  # TODO might be empty for pinboard
+    assert '://' in permalink, permalink
+    parts = [
+        '##',
+        *([prefix] if prefix is not None else []),
+        f'[{title}]({permalink})',
+    ]
+    if url is not None:
+        assert '://' in url, url
+        parts.append(f'    [#]({url})')
+    return ' '.join(parts)
